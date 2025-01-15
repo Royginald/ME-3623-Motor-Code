4e3ae34381e48e782507e5bef7e5c079434fb817
Royginald <77161110+Royginald@users.noreply.github.com>
2025-01-15 14:52:49 -0400

    Added live plotting and other features

--- labjack-t4-test.py
+++ labjack-t4-test.py
@@ -1,28 +1,72 @@
 
 # need the keyboard module: pip3 install keyboard
 # need the labjack module: python -m pip install labjack-ljm
+# need the matplotlib module: pip install matplotlib
 # https://support.labjack.com/docs/python-for-ljm-windows-mac-linux
 
 
 from labjack_T4_functions import motor_class
 
 import keyboard
+import matplotlib.pyplot as plt
+import time
+from numpy import zeros
 
-motor = motor_class()
+# Set up motor
+motor = motor_class(12, 1)
+
+# Plotting Settings
+num_plot_points = 200
+plot_data = zeros((2, num_plot_points))
+time_step = 0.05
+plt.ion()
+fig = plt.figure() 
+ax = fig.add_subplot(111) 
+line1, = ax.plot(plot_data[0][:], plot_data[1][:], 'b-')
+
+# -------------------- Local Variables -------------------- 
+
+setpoint = 100
+
+# --------------------  End Variables  -------------------- 
+
+# Get start Time
+start_time = time.time()
 
 while(True):
-    speed = motor.get_speed_feedback()
+    # Get time and current speed
+    speed = motor.get_speed_feedback() # Returns speed in radians per second
+    new_time = time.time() - start_time
 
     # -------------------- Control Algorithm -------------------- 
 
     u = 5
 
-    # -------------------- End Algorithm -------------------- 
+    # --------------------   End Algorithm   -------------------- 
 
+    # Set voltage applied to motor
     motor.set_motor_voltage(u)
-    print(u)
 
+    # Update data to be plotted
+    plot_data[:, :-1] = plot_data[:, 1:]
+    plot_data[:, -1] = [new_time, speed]
+
+    # Plot new data
+    line1.set_xdata(plot_data[0, :])
+    line1.set_ydata(plot_data[1, :])
+    ax.set_xlim(plot_data[0][0], plot_data[0][-1])
+    ax.set_ylim(min(plot_data[1][:]), max(plot_data[1][:]))
+    fig.canvas.draw() 
+    fig.canvas.flush_events() 
+
+    # End loop if q key is pressed
     if keyboard.is_pressed("q"):
         break
 
+    # wait until time step time has passed
+    while( (time.time() - start_time - new_time) < time_step ):
+        pass
+
+# Turn off motor
 motor.shutdown()
+
--- labjack_T4_functions.py
+++ labjack_T4_functions.py
@@ -1,31 +1,35 @@
 
 from labjack import ljm
+import sys
 
 class motor_class:
 
-    def __init__(self):
+    def __init__(self, max_voltage, min_voltage, dir=False):
         try:
             self.handle = ljm.openS("ANY","ANY","ANY")
         except Exception as e:
             print(e)
+            print("No Labjack Found")
+            sys.exit()
 
         self.SLP_pin = "DIO5"
         self.IN1_pin = "DIO7"
         self.IN2_pin = "DIO6"
 
-        self.Current_feedback_pin = "AIN0"
-        self.Speed_feedback_pin = "AIN2"
+        self.Current_feedback_pin = "AIN2"
+        self.Speed_feedback_pin = "AIN0"
         self.Position_feedback_pin = "AIN3"
 
         self.current_scale = 100 # mA / A
-        self.speed_scale = 0.0286 # rad/s / rad/s
+        self.speed_scale = 1/0.0286 # rad/s / volt
         self.position_scale = 4.77 # rad / volt
 
         self.clock_roll_value = 8000
 
-        self.flip_direction = False
+        self.flip_direction = dir
         self.max_FIO_voltage = 3.3
-        self.max_voltage = 10
+        self.max_voltage = max_voltage
+        self.min_voltage = min_voltage
 
         try:
             ljm.eWriteName(self.handle, "DIO_EF_CLOCK0_ENABLE", 0)
@@ -78,22 +82,34 @@ class motor_class:
         if voltage > self.max_voltage:
             voltage = self.max_voltage
 
-        config = int(voltage / self.max_voltage * self.clock_roll_value)
+        if voltage < self.min_voltage:
+            try:
+                ljm.eWriteName(self.handle, self.SLP_pin, 0) # Set motor driver to sleep mode
+            except Exception as e:
+                print(e)
+        else:
+            config = int(voltage / self.max_voltage * self.clock_roll_value)
 
-        try:
-            ljm.eWriteName(self.handle, pin_off + "_EF_ENABLE", 0)
-            ljm.eWriteName(self.handle, pin_off + "_EF_INDEX", 0)
-            ljm.eWriteName(self.handle, pin_off + "_EF_CONFIG_A", 0)
+            try:
+                ljm.eWriteName(self.handle, self.SLP_pin, 1)# Set motor driver to normal mode
 
-            ljm.eWriteName(self.handle, pin_on + "_EF_ENABLE", 0)
-            ljm.eWriteName(self.handle, pin_on + "_EF_INDEX", 0)
-            ljm.eWriteName(self.handle, pin_on + "_EF_CONFIG_A", config)
-            ljm.eWriteName(self.handle, pin_on + "_EF_ENABLE", 1)
-        except Exception as e:
-            print(e)
+                ljm.eWriteName(self.handle, pin_off + "_EF_ENABLE", 0)
+                ljm.eWriteName(self.handle, pin_off, 0)
+
+                ljm.eWriteName(self.handle, pin_on + "_EF_ENABLE", 0)
+                ljm.eWriteName(self.handle, pin_on + "_EF_INDEX", 0)
+                ljm.eWriteName(self.handle, pin_on + "_EF_CONFIG_A", config)
+                ljm.eWriteName(self.handle, pin_on + "_EF_ENABLE", 1)
+            except Exception as e:
+                print(e)
     
     def shutdown(self): # shutdown the Labjack
         try:
+            ljm.eWriteName(self.handle, self.IN1_pin + "_EF_ENABLE", 0)
+            ljm.eWriteName(self.handle, self.IN2_pin + "_EF_ENABLE", 0)
+            ljm.eWriteName(self.handle, self.IN1_pin, 0)
+            ljm.eWriteName(self.handle, self.IN2_pin, 0)
+            ljm.eWriteName(self.handle, self.SLP_pin, 0)
             ljm.close(self.handle)
         except Exception as e:
             print(e)
