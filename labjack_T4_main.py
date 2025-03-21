d833a17b62decc30a98487449f10710440411421
Royginald <77161110+Royginald@users.noreply.github.com>
2025-03-21 08:42:43 -0300

    Improved setpoint profile and added more plotting settings

--- labjack_T4_main.py
+++ labjack_T4_main.py
@@ -11,21 +11,30 @@ import keyboard
 import matplotlib.pyplot as plt
 import time
 import csv
-from numpy import zeros, concatenate, savetxt, transpose, sin
+from numpy import zeros, concatenate, savetxt, transpose, sin, cos
 
 # Set up motor
 motor = motor_class(3, 10)
 
 # Plotting Settings
-num_plot_points = 400
-plot_data = zeros((2, num_plot_points))
+plt.close()
+num_plot_points = 900
+plot_data = zeros((3, num_plot_points))
 time_step = 0.05
 plt.ion()
 fig = plt.figure() 
+# fig.set_size_inches(16, 9)
+mngr = plt.get_current_fig_manager()
+mngr.window.setGeometry(50,100,1800,900)
+
+# fig.canvas.manager.window.attributes('-topmost', 1)
 ax = fig.add_subplot(111) 
 ax.grid(True)
 line1, = ax.plot(plot_data[0][:], plot_data[1][:], 'b-')
+line2, = ax.plot(plot_data[0][:], plot_data[2][:], 'r-')
 y_range = [0, 0]
+
+fig.tight_layout()
 
 # -------------------- Local Variables -------------------- 
 
@@ -37,23 +46,25 @@ save_data = True
 
 sum = 0
 
+ts = [5, 10, 15, 20, 25, 27.5, 28.75]
+
 def setpoint(t):
-    if t > 40:
-        return 20*sin(10*t - 2*t**2 )
-    elif t > 32.5:
-        return 90 + 16*t
-    elif t > 30:
-        return 130 - 8*t
-    elif t > 25:
-        return 90 + 4*t
-    elif t > 15:
-        return 90
-    elif t > 10:
-        return 110
-    elif t > 5:
-        return 140
-    else:
+    if t > ts[6]:
+        return 30*cos(0.05*(t-ts[6]) + 0.1*(t-ts[6])**2 ) + 120
+    elif t > ts[5]:
+        return 60 + 72*(t-ts[5]) # end at 150
+    elif t > ts[4]:
+        return 150 - 36*(t-ts[4]) # end at 60
+    elif t > ts[3]:
+        return 60 + 18*(t-ts[3]) # end at 150
+    elif t > ts[2]:
+        return 60
+    elif t > ts[1]:
+        return 120
+    elif t > ts[0]:
         return 80
+    else:
+        return 100
 
 
 # --------------------  End Variables  -------------------- 
@@ -81,17 +92,21 @@ while(True):
 
     # Update data to be plotted
     plot_data[:, :-1] = plot_data[:, 1:]
-    plot_data[:, -1] = [new_time, speed]
+    plot_data[:, -1] = [new_time, speed, setpoint(new_time)]
 
     # Plot new data
     line1.set_xdata(plot_data[0, :])
     line1.set_ydata(plot_data[1, :])
+    line2.set_xdata(plot_data[0, :])
+    line2.set_ydata(plot_data[2, :])
+    
     ax.set_xlim(plot_data[0][0], plot_data[0][-1])
     y_range = [min(concatenate( ([y_range[0]], plot_data[1][:]) )), max(concatenate( ([y_range[1]], plot_data[1][:]) ))]
     ax.set_ylim(y_range[0]*1.1, y_range[1]*1.1)
     
     fig.canvas.draw() 
     fig.canvas.flush_events() 
+    plt.show()
     
     # End loop if q key is pressed
     if keyboard.is_pressed("q"):
